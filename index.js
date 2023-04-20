8e2062b5be983c1088c03cdbe42b8e0e8892c8e2
harsha <harshavardhangoda27@gmail.com>
2023-04-20 09:26:33 +0000

    congratulations card page

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 const element = (
-    // Write your code here.
-  )
-  
-ReactDOM.render(element, document.getElementById('root'))
+  // Write your code here.
+  <div className="container">
+    <h1 className="head">Congratulations</h1>
+    <div className="card">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png"
+        className="image"
+      />
+      <h1 className="card-head">Kiran V</h1>
+      <p className="card-para">
+        Vishnu institute of Educational and Technology Bhimavaram
+      </p>
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png"
+        className="image"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
